7133f95f9f088dfc77bfbc278cf218549e32d67b
jamesmilner20 <jamesduah20@gmail.com>
2018-06-19 17:13:54 +0000

    added devless backend to the site

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
-  $("#addRef").click(function(event){
-    event.preventDefault();
-      $('#addreference').append('<div class="form-row">'+
-        '<div class="form-group col-md-4">'+
-          '<label for="inputName9">Name:</label>'+
-          '<input type="text" class="form-control" id="inputName6">'+
-        '</div>'+
-
-        '<div class="form-group col-md-4">'+
-          '<label for="inputMobileNo8">E-mail:</label>'+
-          '<input type="email" class="form-control" id="inputMobileNo8" placeholder="e-mail">'+
-        '</div>'+
-
-        '<div class="form-group col-md-4">'+
-          '<label for="inputMobileNo9">Contact Number:</label>'+
-          '<input type="number" class="form-control" id="inputMobileNo9" placeholder="02********">'+
-        '</div>'+'</div>'
-
-      )
-
-  });
-});
+// $(document).ready(function(){
+//   $("#addRef").click(function(event){
+//     event.preventDefault();
+//       $('#addreference').append('<div class="form-row">'+
+//         '<div class="form-group col-md-4">'+
+//           '<label for="inputName9">Name:</label>'+
+//           '<input type="text" class="form-control" id="inputName6">'+
+//         '</div>'+
+//
+//         '<div class="form-group col-md-4">'+
+//           '<label for="inputMobileNo8">E-mail:</label>'+
+//           '<input type="email" class="form-control" id="inputMobileNo8" placeholder="e-mail">'+
+//         '</div>'+
+//
+//         '<div class="form-group col-md-4">'+
+//           '<label for="inputMobileNo9">Contact Number:</label>'+
+//           '<input type="number" class="form-control" id="inputMobileNo9" placeholder="02********">'+
+//         '</div>'+'</div>'
+//
+//       )
+//
+//   });
+// });
