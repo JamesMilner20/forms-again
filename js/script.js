1a2b0a277325636009300215c1283c0058ef71da
jamesmilner20 <jamesduah20@gmail.com>
2018-06-19 09:52:41 +0000

    modification made to the reference tab

--- js/script.js
+++ js/script.js
@@ -2,23 +2,21 @@ $(document).ready(function(){
   $("#addRef").click(function(event){
     event.preventDefault();
       $('#addreference').append('<div class="form-row">'+
-          '<div class="form-group col-md-4">'+
-            '<label for="inputName9">Name:</label>'+
-            '<input type="text" class="form-control" id="inputName9">'+
-          '</div>'+
+        '<div class="form-group col-md-4">'+
+          '<label for="inputName9">Name:</label>'+
+          '<input type="text" class="form-control" id="inputName6">'+
+        '</div>'+
 
-          '<div class="form-group col-md-4">'+
-            '<label for="inputMobileNo8">E-mail:</label>'+
-            '<input type="email" class="form-control" id="inputMobileNo8" placeholder="e-mail">'+
-          '</div>'+
+        '<div class="form-group col-md-4">'+
+          '<label for="inputMobileNo8">E-mail:</label>'+
+          '<input type="email" class="form-control" id="inputMobileNo8" placeholder="e-mail">'+
+        '</div>'+
 
-          '<div class="form-group col-md-4">'+
-            '<label for="inputMobileNo9">Contact Number:</label>'+
-            '<input type="number" class="form-control" id="inputMobileNo9" placeholder="02********">'+
-          '</div>'+
-          '<div class="form-row" id="addreference">'+
-          '</div>'+
-        '</div>'
+        '<div class="form-group col-md-4">'+
+          '<label for="inputMobileNo9">Contact Number:</label>'+
+          '<input type="number" class="form-control" id="inputMobileNo9" placeholder="02********">'+
+        '</div>'+'</div>'
+
       )
 
   });
